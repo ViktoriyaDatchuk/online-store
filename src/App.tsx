14fff05a3693ad46cbbc6dd55bbea4f300a08c49
proboynick <kuzmi495@mail.ru>
2022-12-20 11:54:57 +0300

    refactor: Product component from class-declaration to functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import {Product} from './components/product/Product'
+
+const prodData = {
+  brand: "Apple",
+  category: "smartphones",
+  description: "An apple mobile which is nothing like apple",
+  discountPercentage: 12.96,
+  id: 1,
+  images: [
+    'https://i.dummyjson.com/data/products/1/1.jpg', 
+    'https://i.dummyjson.com/data/products/1/2.jpg', 
+    'https://i.dummyjson.com/data/products/1/3.jpg', 
+    'https://i.dummyjson.com/data/products/1/4.jpg', 
+    'https://i.dummyjson.com/data/products/1/thumbnail.jpg'
+  ],
+  price: 549,
+  rating: 4.69,
+  stock: 94,
+  thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+  title: "iPhone 9"
+}
 
 function App() {
   return (
-    <h1></h1>
+    <React.Fragment>
+      <h1>Hello</h1>
+      <Product dataset={prodData}/>
+    </React.Fragment>
   )
 }
 
--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import "./product.css"
 
-class Product extends React.Component<{dataset:ProductResponse}> {
-
-  render() {
-    return (
-      <div className='product-container'>
-        <div style={{backgroundImage: `url(${this.props.dataset.thumbnail})`}} className = 'product__img'></div>
-        <div className='product__description'>
-          <span className='product__title'>{this.props.dataset.title}</span>
-          <p className='product__description-text'>{this.props.dataset.description}</p>
-          <div className='product__raiting'>
-            <div className='product__rating_active' style={{width: `calc(100%*(${this.props.dataset.rating}/5))`}}></div>
-            <span>{this.props.dataset.rating}</span>
-          </div>
-        </div>
-        <div className='product__price'>
-          <span>From {this.props.dataset.price} Or</span>
-          <a href="#" className='add'>Add to cart</a>
+export function Product(props:{dataset:ProductResponse}) {
+  return (
+    <div className='product-container'>
+      <div style={{backgroundImage: `url(${props.dataset.thumbnail})`}} className = 'product__img'></div>
+      <div className='product__description'>
+        <span className='product__title'>{props.dataset.title}</span>
+        <p className='product__description-text'>{props.dataset.description}</p>
+        <div className='product__raiting'>
+          <div className='product__rating_active' style={{width: `calc(100%*(${props.dataset.rating}/5))`}}></div>
+          <span>{props.dataset.rating}</span>
         </div>
       </div>
-    )
-  }
+      <div className='product__price'>
+        <span>From {props.dataset.price} Or</span>
+        <a href="#" className='add'>Add to cart</a>
+      </div>
+    </div>
+  )
 }
 
 interface ProductResponse {
@@ -37,5 +34,3 @@ interface ProductResponse {
   thumbnail: string,
   title: string,
 }
-
-export default Product
