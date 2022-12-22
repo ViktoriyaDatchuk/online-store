99e46955742828d2ae0d5c23ee7f8cae37af38cf
proboynick <kuzmi495@mail.ru>
2022-12-22 19:16:15 +0300

    refactor: project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
 import React from "react";
-import { Product } from "./components/product/Product";
-
-const prodData = {
-  brand: "Apple",
-  category: "smartphones",
-  description: "An apple mobile which is nothing like apple",
-  discountPercentage: 12.96,
-  id: 1,
-  images: [
-    "https://i.dummyjson.com/data/products/1/1.jpg",
-    "https://i.dummyjson.com/data/products/1/2.jpg",
-    "https://i.dummyjson.com/data/products/1/3.jpg",
-    "https://i.dummyjson.com/data/products/1/4.jpg",
-    "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-  ],
-  price: 549,
-  rating: 4.69,
-  stock: 94,
-  thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-  title: "iPhone 9",
-};
 
 function App() {
-  return (
-    <React.Fragment>
-      <Product product={prodData} />
-    </React.Fragment>
-  );
+  return <h1>Hello</h1>;
 }
 
 export default App;
--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./product.css";
 import ProductResponse from "../../interfaces/ProductResponse";
 
