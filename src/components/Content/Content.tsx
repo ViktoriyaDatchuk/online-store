6b8761da640765d9c525c7316c70405e3ee84056
proboynick <kuzmi495@mail.ru>
2022-12-23 20:00:13 +0300

    feat: add search pannel, more products, add some logic

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,12 +1,34 @@
+import ProductResponse from "../../interfaces/ProductResponse";
 import "../Content/Content.css";
+import { Product } from "../product/Product";
 
-export function Content() {
+export function Content(props: { products: ProductResponse[] }) {
+  const { products } = props;
   return (
     <main>
       <div className="mainContainer">
         <div className="filtersSpace"></div>
-        <div className="products">Products</div>
+        <div className="products">
+          <div className="controls">
+            <input
+              type="search"
+              name=""
+              id=""
+              className="products__search-field"
+              placeholder="Search product"
+            />
+          </div>
+          {fillContent(products)}
+        </div>
       </div>
     </main>
   );
+}
+
+function fillContent(prods: ProductResponse[]) {
+  const content = [];
+  for (const prod of prods) {
+    content.push(<Product product={prod} />);
+  }
+  return content;
 }
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,14 @@
 import { Content } from "../../components/Content/Content";
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from "../../components/Header/Header";
+import { productArray } from "../../stubs/ProductStub";
 import "./Main.css";
 
 export function Main() {
   return (
     <div className="wrapper">
       <Header />
-      <Content />
+      <Content products={productArray} />
       <Footer />
     </div>
   );
